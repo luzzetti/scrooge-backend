7d54f1973c7c1b784dde470daa4f1f4290bdc04e
christian <christian.luzzetti@gmail.com>
2025-04-12 20:38:44 +0200

    Initialize database migration setup with Liquibase
    Set up Liquibase for managing database migrations and configured changelog paths in the application.yml. Added initial database schema creation scripts and updated the application to validate schema on startup. Included Liquibase dependency in the project.

---
+++ src/main/resources/db/changelog/migrations/001-init-migration.sql
@@ -0,0 +1,35 @@
+create table bank_accounts
+(
+    id            uuid not null,
+    balance       numeric(38, 2) not null ,
+    iban          varchar(50) not null,
+    mnemonic_name varchar(50) not null ,
+    owner_id      uuid not null ,
+    primary key (id)
+);
+
+create table scrooge_users
+(
+    id    uuid not null,
+    email varchar(50) not null ,
+    primary key (id)
+);
+
+create table transactions
+(
+    id                uuid not null,
+    amount            numeric(38, 2) not null ,
+    created_at        timestamptz not null,
+    source_account_id uuid not null ,
+    target_account_id uuid not null ,
+    primary key (id)
+);
+
+alter table if exists bank_accounts
+    add constraint FK_BANK_ACCOUNT_TO_USERS foreign key (owner_id) references scrooge_users;
+
+alter table if exists transactions
+    add constraint FK_TRANSACTION_TO_BANK_ACCOUNT_SOURCE foreign key (source_account_id) references bank_accounts;
+
+alter table if exists transactions
+    add constraint FK_TRANSACTION_TO_BANK_ACCOUNT_TARGET foreign key (target_account_id) references bank_accounts
